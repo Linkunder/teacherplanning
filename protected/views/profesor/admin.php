5b9b2b0b01034b10b880266eb3261b5d6af05f39
Linkunder <linkunder@hotmail.com>
2016-12-15 06:57:41 -0300

    cosas y reportes usuarios

--- protected/views/profesor/admin.php
+++ protected/views/profesor/admin.php
@@ -5,64 +5,82 @@
 $this->breadcrumbs=array(
 	'Profesores'=>array('index'),
 
-);
+	);
 
 $this->menu=array(
 	array('label'=>'List Profesor', 'url'=>array('index')),
 	array('label'=>'Create Profesor', 'url'=>array('create')),
-);
+	);
 
 Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#profesor-grid').yiiGridView('update', {
-		data: $(this).serialize()
+	$('.search-button').click(function(){
+		$('.search-form').toggle();
+		return false;
 	});
-	return false;
-});
-");
-?>
+	$('.search-form form').submit(function(){
+		$('#profesor-grid').yiiGridView('update', {
+			data: $(this).serialize()
+		});
+		return false;
+	});
+	");
+	?>
 
-<h1>Profesores</h1>
+	<h1>Profesores</h1>
 
-<p>
-Opcionalmente puedes ingresar un operador de comparación (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) al inicio de cada uno de los valores de busqueda para especificar que comparación debe hacerse.
-</p>
+	<p>
+		Opcionalmente puedes ingresar un operador de comparación (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+		or <b>=</b>) al inicio de cada uno de los valores de busqueda para especificar que comparación debe hacerse.
+	</p>
 
-<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+	<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button')); ?>
+	<div class="search-form" style="display:none">
+		<?php $this->renderPartial('_search',array(
+			'model'=>$model,
+			)); ?>
+		</div><!-- search-form -->
 
-<?php 
+		<?php 
 
 
-$this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'profesor-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'nombre',
-		'apellido',
-		'rut',
-		'mail',
-		array(
-				'header'=>'Cursos',
-				'name'=>'idProfesor',
-				'value'=>function($data, $row) use (&$model){
-					 $result = count((Array)(Curso::model()->findAllByAttributes(array('idProfesor' => $data->idProfesor,))));
-					 return ''.$result;
-				},
-				
-				
+		$this->widget('zii.widgets.grid.CGridView', array(
+			'id'=>'profesor-grid',
+			'dataProvider'=>$model->search(),
+			'filter'=>$model,
+			'columns'=>array(
+				'nombre',
+				'apellido',
+				'rut',
+				'mail',
+				array(
+					'header'=>'Cursos',
+					'name'=>'idProfesor',
+					'value'=>function($data, $row) use (&$model){
+						$result = count((Array)(Curso::model()->findAllByAttributes(array('idProfesor' => $data->idProfesor,))));
+						return ''.$result;
+					},
 
-		),
+
+
+					),
+				array(
+					'header'=>'Plan',
+					'name'=>'idProfesor',
+					'value'=>function($data, $row) use (&$model){
+
+						$profesor = $data->itsfree;
+						if($profesor == 1 ){
+							return "De pago";
+						}else{
+							return "Gratis";
+						}
+
+
+					},
+
+
+
+					),
 
 		/*
 		'perfil',
@@ -72,10 +90,6 @@ $this->widget('zii.widgets.grid.CGridView', array(
 		),
 		*/
 
-		                'link'=>array(
-                        'header'=>'',
-                        'type'=>'raw',
-                        'value'=> 'CHtml::button("Plan",array("onclick"=>""))',
-                ), 
-	),
-)); ?>
+
+		),
+		)); ?>
--- protected/views/reporte/reportes.php
+++ protected/views/reporte/reportes.php
@@ -8,19 +8,19 @@ $this->breadcrumbs=array(
 
   <div class="btn-group btn-group-justified">
     <a href="#" class="btn btn-primary col-sm-3">
-      <strong><h2><i class="glyphicon glyphicon-book"></i>&nbsp;12</h2></strong>
+      <strong><h2><i class="glyphicon glyphicon-book"></i>&nbsp;<?php echo $cursos[0]['cantidad']; ?></h2></strong>
       Cursos
     </a>
     <a href="#" class="btn btn-primary col-sm-3">
-     <strong><h2><i class="glyphicon glyphicon-user"></i>&nbsp;12</h2></strong>
+     <strong><h2><i class="glyphicon glyphicon-user"></i>&nbsp;<?php echo $alumnos[0]['cantidad']; ?></h2></strong>
      Alumnos
    </a>
    <a href="#" class="btn btn-primary col-sm-3">
-    <strong><h2><i class="  glyphicon glyphicon-alert"></i>&nbsp;12</h2></strong>
+    <strong><h2><i class="  glyphicon glyphicon-alert"></i>&nbsp;<?php echo $anotaciones[0]['cantidad']; ?></h2></strong>
     Anotaciones
   </a>
   <a href="#" class="btn btn-primary col-sm-3">
-    <strong><h2><i class="glyphicon glyphicon-pencil"></i>&nbsp;12</h2></strong>
+    <strong><h2><i class="glyphicon glyphicon-pencil"></i>&nbsp;<?php echo $evaluaciones[0]['cantidad']; ?></h2></strong>
     Evaluaciones
   </a>
 </div>
@@ -38,21 +38,11 @@ $this->breadcrumbs=array(
           <div id="panel-element-489495" class="panel-collapse collapse">
             <div class="panel-body">
             <div class="block center">
-            <?php $this->renderPartial('_graficosCursos')?>
+            <?php $this->renderPartial('_graficosCursos',array('graficoAlumnosCurso'=>$graficoAlumnosCurso,'graficoCursoInstitucion' => $graficoCursoInstitucion, 'cursoEvaluacion' => $cursoEvaluacion, ));?>
             </div>
             </div>
           </div>
         </div>
-        <div class="panel panel-default">
-          <div class="panel-heading">
-           <a class="panel-title" data-toggle="collapse" data-parent="#panel-72128" href="#panel-element-348463">Alumnos</a>
-         </div>
-         <div id="panel-element-348463" class="panel-collapse collapse">
-          <div class="panel-body">
-
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </div>
